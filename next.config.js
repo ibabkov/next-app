8f1c8192e2e670974eb3c323771de86c1078afe2
ibabkov <i.babkov.dev@gmail.com>
2024-01-29 12:02:13 -0500

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,31 @@
+const IS_DEV_MODE = process.env.NODE_ENV !== 'production';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+    dirs: ['pages', 'src'],
+  },
+  publicRuntimeConfig: {
+    IS_DEV_MODE: IS_DEV_MODE,
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.glsl$/,
+      loader: 'webpack-glsl-loader',
+    });
+
+    return config;
+  },
+};
+
+module.exports = (phase, { defaultConfig }) => {
+  const plugins = [];
+
+  return plugins.reduce((acc, plugin) => plugin(acc), {
+    ...defaultConfig,
+    ...nextConfig,
+  });
+};
---
+++ src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { Html, Head, Main, NextScript } from 'next/document';
+
+import { LANGUAGE } from '../constants/app';
+
+function Document() {
+  return (
+    <Html lang={LANGUAGE}>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
+export default Document;
